3afbab1183e1124b578bae7a21c4833dfc1f47f6
booktest <fonto.trg@gmail.com>
2018-09-14 11:58:44 +0300

    #refactor bug with date

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExceed;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
@@ -12,14 +13,17 @@ import java.util.stream.Collectors;
 public class UserMealsUtil {
     public static void main(String[] args) {
         List<UserMeal> mealList = Arrays.asList(
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,10,0), "Завтрак", 500),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,13,0), "Обед", 1000),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,10,0), "Завтрак", 300),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,13,0), "Обед", 700),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,20,0), "Ужин", 500),
+                new UserMeal(LocalDateTime.of(2017, Month.MAY, 30,10,0), "Завтрак", 500),
+                new UserMeal(LocalDateTime.of(2017, Month.MAY, 30,13,0), "Обед", 1050),
+                new UserMeal(LocalDateTime.of(2017, Month.MAY, 30,20,0), "Ужин", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,10,0), "Завтрак", 1000),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510));
-        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(19,59), 2000)
-        .forEach(meal -> System.out.println(meal.getDescription()));
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 500));
+        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,59), 2000)
+        .forEach(meal -> System.out.println(meal.getDateTime()+" : "+meal.getDescription()+" --- "+meal.isExceed()));
 //        .toLocalDate();
 //        .toLocalTime();
     }
@@ -31,8 +35,8 @@ public class UserMealsUtil {
             throw new IllegalArgumentException("List is empty");
         }
         List<UserMealWithExceed> filterList = new ArrayList<>();
-        mealList.stream().collect(Collectors.groupingBy(userMeal -> userMeal.getDateTime()
-                        .getDayOfYear())).entrySet().forEach(meals ->
+        mealList.stream().collect(Collectors.groupingBy(userMeal -> userMeal.getDateTime().toLocalDate()
+                        )).entrySet().forEach(meals ->
                             meals.getValue().forEach(meal ->
                            filterList.add(new UserMealWithExceed(
                                    meal.getDateTime(),
@@ -46,7 +50,7 @@ public class UserMealsUtil {
                         .collect(Collectors.toCollection(ArrayList::new));
             }
 
-    private static boolean normalsCalories(Map.Entry<Integer, List<UserMeal>> integerListEntry, int caloriesPerDay) {
+    private static boolean normalsCalories(Map.Entry<LocalDate, List<UserMeal>> integerListEntry, int caloriesPerDay) {
         return integerListEntry.getValue().stream().mapToInt(UserMeal::getCalories).sum() <= caloriesPerDay;
     }
 }
