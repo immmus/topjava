92fec484626671314f66d4da35808d792bbf8fb3
booktest <fonto.trg@gmail.com>
2018-10-13 16:37:21 +0300

    #lesson two

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -14,24 +14,15 @@ public class Meal {
 
     private final int calories;
 
-    public Meal(Integer id, LocalDateTime dateTime, String description, int calories) {
-        this.dateTime = dateTime;
-        this.description = description;
-        this.calories = calories;
-        this.id = id;
-    }
     public Meal(LocalDateTime dateTime, String description, int calories) {
+                this(null, dateTime, description, calories);
+         }
+
+    public Meal(Integer id, LocalDateTime dateTime, String description, int calories) {
+        this.id = id;
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
     }
 
     public LocalDateTime getDateTime() {
