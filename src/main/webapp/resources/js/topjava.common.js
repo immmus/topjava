3ec9bb2df67fa8de21a2c1d7390733a44ca579b0
booktest <fonto.trg@gmail.com>
2018-12-31 12:34:59 +0300

    #fix bug 2

--- src/main/webapp/resources/js/topjava.common.js
+++ src/main/webapp/resources/js/topjava.common.js
@@ -104,11 +104,9 @@ function successNoty(key) {
 function failNoty(jqXHR) {
     closeNoty();
     // https://stackoverflow.com/questions/48229776
-    const responseJSON = JSON.parse(jqXHR.responseText);
     const errorInfo = JSON.parse(jqXHR.responseText);
     failedNote = new Noty({
-        text: "<span class='fa fa-lg fa-exclamation-circle'></span> &nbsp;" + i18n["common.errorStatus"] + ": " + jqXHR.status + (responseJSON ? "<br>" + responseJSON : ""),
-        text: "<span class='fa fa-lg fa-exclamation-circle'></span> &nbsp;" + i18n["common.errorStatus"] + ": " + jqXHR.status + "<br>" + errorInfo.type + "<br>" + errorInfo.detail,
+        text: "<span class='fa fa-lg fa-exclamation-circle'></span> &nbsp;"+ errorInfo.typeMessage + "<br>" + errorInfo.details.join("<br>"),
         type: "error",
         layout: "bottomRight"
     }).show();
